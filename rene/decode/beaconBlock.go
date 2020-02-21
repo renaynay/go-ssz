8eca3fa301e38d651585f9c2cee25a4032f6a200
renaynay <41963722+renaynay@users.noreply.github.com>
2020-02-21 18:42:54 +0100

    some unmarshal for proposerslashing, not finished

--- rene/decode/beaconBlock.go
+++ rene/decode/beaconBlock.go
@@ -1,1 +1,59 @@
 package decode
+
+import (
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
+	"github.com/renaynay/go-ssz/rene/basic-types/decode"
+)
+
+// it would be serialized as:
+//<proposer_index>
+//<signed_header_1.message.slot>
+//<signed_header_1.message.parent_root>
+//<signed_header_1.message.state_root>
+//<signed_header_1.message.body_root>
+//<signed_header_1.signature>
+//<signed_header_2.message.slot>
+//<signed_header_2.message.parent_root>
+//<signed_header_2.message.state_root>
+//<signed_header_2.message.body_root>
+//<signed_header_2.signature>
+func UnmarshalProposerSlashing(in []byte, ps *ethpb.ProposerSlashing) error {
+	rawProposerIndex := in[:8]
+	proposerIndex, err := decode.UnmarshalUint64(rawProposerIndex)
+	if err != nil {
+		return err
+	}
+	ps.ProposerIndex = proposerIndex
+
+	headerLen := (len(in) - 8) / 2 // TODO how to know the length?
+
+	rawHeader1 := in[8:8+headerLen] // TODO how to know the length?
+	var header1 ethpb.SignedBeaconBlockHeader
+
+	err = UnmarshalSignedBeaconBlockHeader(rawHeader1, &header1)
+	if err != nil {
+		return err
+	}
+	ps.Header_1 = &header1
+
+	rawHeader2 := in[8+headerLen:] // TODO how to know the length?
+	var header2 ethpb.SignedBeaconBlockHeader
+
+	err = UnmarshalSignedBeaconBlockHeader(rawHeader2, &header2)
+	if err != nil {
+		return err
+	}
+	ps.Header_2 = &header2
+
+	return nil
+}
+
+func UnmarshalSignedBeaconBlockHeader(in []byte, sbbh *ethpb.SignedBeaconBlockHeader) error {
+
+	return nil
+}
+
+func UnmarshalBeaconBlockHeader(in []byte, bbh *ethpb.BeaconBlockHeader) error {
+
+	return nil
+}
--- rene/encode/beaconBlock.go
+++ rene/encode/beaconBlock.go
@@ -5,35 +5,36 @@ import (
 	"github.com/renaynay/go-ssz/rene/basic-types/encode"
 )
 
+// TODO this implementation is incorrect
 func MarshalBeaconBlock(bb *ethpb.BeaconBlock) ([]byte, error) {
-	out := make([]byte, 0)
-	offset := uint64(0)
+	//out := make([]byte, 0)
+	//offset := uint64(0)
+	//
+	//marshaledSlot := encode.MarshalUint64(bb.Slot)
+	//out = append(out, marshaledSlot...)
+	//offset++
+	//
+	//marshaledParentRootLen := encode.MarshalUint32(uint32(len(bb.ParentRoot)))
+	//out = append(out, marshaledParentRootLen...)
+	//out = append(out, bb.ParentRoot...)
+	//offset += uint64(len(marshaledParentRootLen) + len(bb.ParentRoot))
+	//
+	//marshaledStateRootLen := encode.MarshalUint32(uint32(len(bb.StateRoot)))
+	//out = append(out, marshaledStateRootLen...)
+	//out = append(out, bb.StateRoot...)
+	//offset += uint64(len(marshaledStateRootLen) + len(bb.StateRoot))
+	//
+	//marshaledBody, err := MarshalBeaconBlockBody(bb.Body)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//out = append(out, marshaledBody...)
+	//offset += uint64(len(marshaledBody))
+	//
+	//marshaledOffset := encode.MarshalUint64(offset)
+	//marshaledOffset = append(marshaledOffset, out...)
 
-	marshaledSlot := encode.MarshalUint64(bb.Slot)
-	out = append(out, marshaledSlot...)
-	offset++
-
-	marshaledParentRootLen := encode.MarshalUint32(uint32(len(bb.ParentRoot)))
-	out = append(out, marshaledParentRootLen...)
-	out = append(out, bb.ParentRoot...)
-	offset += uint64(len(marshaledParentRootLen) + len(bb.ParentRoot))
-
-	marshaledStateRootLen := encode.MarshalUint32(uint32(len(bb.StateRoot)))
-	out = append(out, marshaledStateRootLen...)
-	out = append(out, bb.StateRoot...)
-	offset += uint64(len(marshaledStateRootLen) + len(bb.StateRoot))
-
-	marshaledBody, err := MarshalBeaconBlockBody(bb.Body)
-	if err != nil {
-		return nil, err
-	}
-	out = append(out, marshaledBody...)
-	offset += uint64(len(marshaledBody))
-
-	marshaledOffset := encode.MarshalUint64(offset)
-	marshaledOffset = append(marshaledOffset, out...)
-
-	return marshaledOffset, nil
+	return []byte{}, nil // TODO
 }
 
 // it would be serialized as:
